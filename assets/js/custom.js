27a79665247d7e8f7067a9e07be2d9d99cf88eb0
P-Krebs <pkrebs@fastmail.com>
2016-05-18 18:13:39 -0500

    Custom Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,7 +14,6 @@ $(document).ready(function () {
 		var users = []
 
 	    $.ajax({url: "http://ec2-54-187-183-27.us-west-2.compute.amazonaws.com:8080/olayit_api/Users", success: function(result){
-	    
 	    	users = result; 
 	    	console.log(users);
 	    
